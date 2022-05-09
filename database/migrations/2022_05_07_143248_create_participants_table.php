1586a4c79e1d5046de3928b7c082b36fb353a31f
Naztarhex <qhmed0826@gmail.com>
2022-05-09 22:41:04 +0200

    My Commit

--- database/migrations/2022_05_07_143248_create_participants_table.php
+++ database/migrations/2022_05_07_143248_create_participants_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('participants', function (Blueprint $table) {
+        Schema::create('participate', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->String('name');
